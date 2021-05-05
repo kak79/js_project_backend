1aa44d2a6a02a6594a2720633dbfbfed2ed8d5c6
Kimberly Kohel-Hayes <kohelka@gmail.com>
2021-05-05 07:11:42 -0500

    took out personal comments, psudocode and prys

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,19 +1,16 @@
 class IngredientsController < ApplicationController
   before_action :set_ingredient, only: [:show, :update, :destroy]
 
-  # GET /ingredients
   def index
     @ingredients = Ingredient.all
 
     render json: @ingredients
   end
 
-  # GET /ingredients/1
   def show
     render json: @ingredient
   end
 
-  # POST /ingredients
   def create
     @ingredient = Ingredient.new(ingredient_params)
 
@@ -24,7 +21,6 @@ class IngredientsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /ingredients/1
   def update
     if @ingredient.update(ingredient_params)
       render json: @ingredient
@@ -33,18 +29,16 @@ class IngredientsController < ApplicationController
     end
   end
 
-  # DELETE /ingredients/1
   def destroy
     @ingredient.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_ingredient
       @ingredient = Ingredient.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def ingredient_params
       params.require(:ingredient).permit(:name, :measurement, :recipe_ids)
     end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,22 +1,18 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: [:show, :update, :destroy]
 
-  # GET /recipes
   def index
     @recipes = Recipe.all
 
-    # using include allows us to show the ingredients that belong_to a given recipe
     render json: @recipes,
       include: [:ingredients], except: [:created_at, :updated_at]
   end
 
-  # GET /recipes/1
   def show
     render json: @recipe,
     include: [:ingredients], except: [:created_at, :updated_at]  
   end
 
-  # POST /recipes
   def create
     @recipe = Recipe.new(recipe_params)
     
@@ -27,20 +23,13 @@ class RecipesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /recipes/1
   def update
-    # binding.pry
-    # find recipe id in db and save to var
     recID = Recipe.find(params[:id].to_i)
-    # access ingredients through recipe id variable
     @ingredient = Ingredient.create(name: params[:ingredients_attributes][:name])
     @ingredient.measurement = params[:ingredients_attributes][:measurement]
     @ingredient.save
     recID.ingredients
     recID.ingredients << @ingredient
-    # << ingredient into ingredients [] 
-    # save recipe.id.ingredients
-    # if true everything after if line will work
     if recID.save
       render json: recID,
       include: [:ingredients], except: [:created_at, :updated_at]  
@@ -49,18 +38,15 @@ class RecipesController < ApplicationController
     end
   end
 
-  # DELETE /recipes/1
   def destroy
     @recipe.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_recipe
       @recipe = Recipe.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def recipe_params
       
       params.require(:recipe).permit(:id, :title, :instructions, :ingredients_attributes => [])
